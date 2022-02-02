631689e1586f0e5810618119067cf46059248dba
VikasBenki <vikasmsbenki@gmail.com>
2022-02-02 21:39:18 +0530

    [vikasBenki] adding the Find the maximum int value

--- GenericsMaximum/GenericsMaximum/Program.cs
+++ GenericsMaximum/GenericsMaximum/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GenericsMaximum.Findmaximum;
 
 namespace GenericsMaximum
 {
@@ -10,6 +11,15 @@ namespace GenericsMaximum
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello welcome to Generics problem");
+
+            //Instanse variables.
+            int first = 20, second = 300, third = 40;
+
+            //Creating object of FindMaxValueClass.
+            FindMaxValue findMaxValue = new FindMaxValue();
+            Console.WriteLine("Max number is:{0} out of numbers : {1} , {2} , {3}", findMaxValue.FindMaxInteger(first, second, third), first, second, third);
+            Console.ReadLine();
         }
     }
 }
